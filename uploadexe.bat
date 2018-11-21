1c8ad508817bf7354138c737be563fffc848803c
MarsZhang <6671605@qq.com>
2018-11-21 16:41:05 +0800

    windows 4

--- uploadexe.bat
+++ uploadexe.bat
@@ -1,5 +1,7 @@
 @echo %1
 dir myprjwindows
 cd myprjwindows
-cpack --config CPackConfig.cmake
+"C:\Program Files\CMake\bin\cpack.exe" --config CPackConfig.cmake
 dir
+gittoken=%1
+curl -XPOST -H "Content-Type: application/zip" -H "Authorization: token $gittoken" -H "Accept: application/vnd.github.v3+json"  --data-binary @Tutorial-1.0.1-Linux.tar.gz "https://uploads.github.com/repos/makefriend8/Azure_Pipelines_test_c/releases/14070624/assets?name=Tutorial-1.0.1-Linux.tar.gz"
