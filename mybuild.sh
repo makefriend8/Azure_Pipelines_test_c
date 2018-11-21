9fafe3e74e56c8c6fc253b1b8f5b9e6d5cbba5c6
MarsZhang <6671605@qq.com>
2018-11-21 14:31:02 +0800

    linux 16

--- mybuild.sh
+++ mybuild.sh
@@ -8,7 +8,7 @@ ls
 make
 ls
 cpack --config CPackConfig.cmake
-gittoken=%1
+gittoken=$1
 echo $githubtoken
 echo $gittoken
 curl -XPOST -H "Content-Type: application/zip" -H "Authorization: token $gittoken" -H "Accept: application/vnd.github.v3+json"  --data-binary @Tutorial-1.0.1-Linux.sh "https://uploads.github.com/repos/makefriend8/Azure_Pipelines_test_c/releases/14070624/assets?name=Tutorial-1.0.1-Linux.sh"
