73de13d687c3b1e62c417a12d38f911b9441fe5c
MarsZhang <6671605@qq.com>
2018-11-22 11:39:19 +0800

    window 12

--- mycmake.bat
+++ mycmake.bat
@@ -1,6 +1,6 @@
-curl -O https://superb-sea2.dl.sourceforge.net/project/nsis/NSIS%203/3.03/nsis-3.03-setup.exe
-dir
-nsis-3.03-setup.exe /S
+rem curl -O https://superb-sea2.dl.sourceforge.net/project/nsis/NSIS%203/3.03/nsis-3.03-setup.exe
+rem dir
+rem nsis-3.03-setup.exe /S
 @echo helle world
 @echo $(githubtoken)
 @echo %1
--- uploadexe.bat
+++ uploadexe.bat
@@ -4,4 +4,4 @@ cd myprjwindows
 "C:\Program Files\CMake\bin\cpack.exe" --config CPackConfig.cmake
 dir
 gittoken=%1
-curl -XPOST -H "Content-Type: application/zip" -H "Authorization: token $gittoken" -H "Accept: application/vnd.github.v3+json"  --data-binary @Tutorial-1.0.1-Linux.tar.gz "https://uploads.github.com/repos/makefriend8/Azure_Pipelines_test_c/releases/14070624/assets?name=Tutorial-1.0.1-Linux.tar.gz"
+curl -XPOST -H "Content-Type: application/zip" -H "Authorization: token %1" -H "Accept: application/vnd.github.v3+json"  --data-binary @Tutorial-1.0.1-win64.exe "https://uploads.github.com/repos/makefriend8/Azure_Pipelines_test_c/releases/14070624/assets?name=Tutorial-1.0.1-win64.exe"
