12cc92ab087ca3360c72b86a6a63d0c3808ed986
Mars Zhang <6671605@qq.com>
2020-01-20 15:14:48 +0800

    fix7

--- mycmake.bat
+++ mycmake.bat
@@ -1,9 +1,5 @@
-curl -O https://nchc.dl.sourceforge.net/project/nsis/NSIS%203/3.05/nsis-3.05-setup.exe
-dir
-nsis-3.05-setup.exe /S
+choco install nsis -y
 @echo helle world
-@echo $(githubtoken)
-@echo %1
 mkdir myprjwindows
 cd myprjwindows
 cmake .. -G "Visual Studio 15 Win64"
