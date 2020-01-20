d9b3b12ea4482531fc024508f603bc1765aa05b9
Mars Zhang <6671605@qq.com>
2020-01-20 14:57:06 +0800

    fix4

--- mycmake.bat
+++ mycmake.bat
@@ -8,4 +8,5 @@ mkdir myprjwindows
 cd myprjwindows
 cmake .. -G "Visual Studio 15 Win64"
 cmake --build . --config Release
-cpack.exe  --config CPackConfig.cmake
+dir
+cpack  --config CPackConfig.cmake
