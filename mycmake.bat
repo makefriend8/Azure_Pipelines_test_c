9e26f975c7b8cc244a31cbacbb9b5a5c63bf83ca
Mars Zhang <6671605@qq.com>
2020-01-20 15:34:34 +0800

    fix8

--- mycmake.bat
+++ mycmake.bat
@@ -1,8 +1,9 @@
-choco install nsis -y
 @echo helle world
 mkdir myprjwindows
 cd myprjwindows
 cmake .. -G "Visual Studio 15 Win64"
 cmake --build . --config Release
 dir
+cpack --help
+cpack --version
 cpack  --config CPackConfig.cmake
